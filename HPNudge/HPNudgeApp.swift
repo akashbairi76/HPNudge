d12cde36cce1e1740c7c6f1546a1d13b8b831ea8
akash <akash.bairi@housing.com>
2024-04-29 15:05:28 +0530

    Initial Commit

---
+++ HPNudge/HPNudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPNudgeApp.swift
+//  HPNudge
+//
+//  Created by Bairi Akash on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct HPNudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
