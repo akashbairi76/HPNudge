eab2b6f58b583fa4e42bfb807aed4859a727ce30
akash <akash.bairi@housing.com>
2024-06-04 08:54:59 +0530

    first commit

--- HPNudge/ContentView.swift
+++ HPNudge/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  HPNudge
-//
-//  Created by Bairi Akash on 29/04/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
@@ -13,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, xcloud!")
         }
         .padding()
     }
